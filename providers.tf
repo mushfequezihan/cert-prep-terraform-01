82977a10ce7ffba00da0b5200b7c1c440dc8819c
mushfequezihan <mushfeque.zihan@gmail.com>
2025-04-07 23:31:12 -0400

    inint commit

--- providers.tf
+++ providers.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.31.0"
+      version = ">=5.31.0"
     }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-  profile = "Helen"
+  profile = "terra-admin"
   # Configuration options
 }
